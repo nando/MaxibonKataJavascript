08f6b53daa5f43fe9eed1ffb8b6b2041bff3e5f8
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-02-05 09:18:57 +0100

    assign # of maxibons specified to every developer property

--- src/Developer.test.js
+++ src/Developer.test.js
@@ -2,6 +2,17 @@
 
 // import Developer from "./Developer"
 class Developer {
+  static developers() {
+    const karumies = {
+      pedro:  new Developer( 'Pedro',  3 ),
+      fran:   new Developer( 'Fran',   1 ),
+      davide: new Developer( 'Davide', 0 ),
+      sergio: new Developer( 'Sergio', 2 ),
+      jorge:  new Developer( 'Jorge',  1 )
+    };
+    return karumies;
+  }
+
   constructor ( name, maxibons_to_grab ) {
     this.name = name;
     this.maxibons_to_grab = Math.max( 0, maxibons_to_grab );
@@ -10,6 +21,11 @@ class Developer {
   maxibonsToGrab() {
     return this.maxibons_to_grab;
   }
+
+  static get( developer_name ) {
+    return Developer.developers()[ developer_name ]
+  }
+
 }
 
 const assert = require('assert');
@@ -41,6 +57,21 @@ describe('Developer', () => {
       const dev = new Developer( record.name,
                                  record.maxibons_to_grab );
       return dev.name === record.name
+    })
+  })
+
+  it('should assign the number of maxibons specified to every developer', () => {
+    let maxibons_to_grab
+    const karumies = {
+      pedro: 3,
+      fran: 1,
+      davide: 0,
+      sergio: 2,
+      jorge: 1
+    }
+    Object.keys( karumies ).forEach( function (key) {
+      maxibons_to_grab = karumies[ key ]
+      assert.equal( maxibons_to_grab, Developer.get( key ).maxibonsToGrab() )
     })
   })
 })
