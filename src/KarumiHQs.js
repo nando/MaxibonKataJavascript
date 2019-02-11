12299b63e44df6f0f7eacc15a558a3a720dcfeb6
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-02-11 12:42:42 +0100

    always more than two maxibons property

--- src/KarumiHQs.js
+++ src/KarumiHQs.js
@@ -1,9 +1,30 @@
 export default class KarumiHQs {
+  static MIN_MAXIBONS() { return 3; };
+  static MAXIBONS_TO_BUY() { return 10; };
+
   constructor() {
     this.maxibons_left = 10;
   }
 
   maxibonsLeft() {
     return this.maxibons_left;
+  }
+
+  openFridge( developer ) {
+    this.grab_maxibons( developer )
+
+    if( this.maxibons_left < KarumiHQs.MIN_MAXIBONS() ) {
+      this.buy_maxibons();
+    }
+
+    return this.maxibons_left;
+  }
+
+  grab_maxibons( developer ) {
+    this.maxibons_left =- Math.max( 0, developer.maxibonsToGrab() );
+  }
+
+  buy_maxibons() {
+    this.maxibons_left =+ KarumiHQs.MAXIBONS_TO_BUY()
   }
 }
--- test/KarumiHQs.test.js
+++ test/KarumiHQs.test.js
@@ -3,6 +3,7 @@ import { jsc,
          unpredictableDev,
          karumiDev } from './helper'
 import KarumiHQs from '../src/KarumiHQs'
+import Developer from '../src/Developer'
 
 describe('KarumiHQs', () => {
   it('should start the day with 10 maxibons', () => {
@@ -10,5 +11,17 @@ describe('KarumiHQs', () => {
     expect( karumi_hqs.maxibonsLeft() ).
       to.
       equal( 10 );
+  })
+
+  it('should always has more than two maxibons in the fridge', () => {
+    jsc.assertForall( unpredictableDev,
+                      ( record ) => {
+      const hqs = new KarumiHQs();
+      const dev = new Developer( record.name,
+                                 record.maxibons_to_grab );
+
+      hqs.openFridge( dev );
+      return hqs.maxibonsLeft() > 2;
+    })
   })
 })
